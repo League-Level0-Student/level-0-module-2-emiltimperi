5e3c5c04a0f5b47875cc552a32f1a2d7b2e734a9
timperi.emil <timperi.emil@SEMS-259-08.SEMS259.LAB>
2018-01-24 14:57:24 -0800

    Random

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -11,7 +11,7 @@ public class Validation {
 	public static void main(String[] args) {
 
 		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
-		
+		for (int j = 0; j < 10; j++) {
 		int randomNumber = new Random().nextInt(5);
 		
 		// 2. On paper, write all the numbers that get printed when you run this class 10 times
@@ -19,7 +19,23 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
-
+		
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "You Have A Nice Shirt");
+		}
+		else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "Your hair looks nice");
+		}
+		else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "You look really nice today");
+		}
+		else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "I like your hair");
+		}
+		else  {
+			JOptionPane.showMessageDialog(null, "Have a nice day");
+		}
+		}
 		// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
